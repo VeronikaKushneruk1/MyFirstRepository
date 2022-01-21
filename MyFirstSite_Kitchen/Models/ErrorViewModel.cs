4fceae40d53ff90b9af6057902b930422c511484
Veronika Kushneruk <veronika.kushneruk@outlook.com>
2022-01-21 18:44:53 +0300

    My first commit

---
+++ MyFirstSite_Kitchen/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFirstSite_Kitchen.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
